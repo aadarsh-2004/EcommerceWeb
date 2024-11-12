365d74d2ff310ff7b383d06aa5e4ce0d0b787554
aadarsh-2004 <2020aadarshsoni@gmail>
2024-11-12 15:25:07 +0530

    secondd commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import Navbar from './customer/components/Navbar'
 import Homepage from './customer/pages/Homepage'
 import Footer from './customer/components/Footer'
 import Calendar from './customer/components/Calendar'
+import Student from './customer/components/Student'
 function App() {
   return (
     <div>
         <>
-        {/* <Navbar/>
-        <Homepage/>
-        <Footer/> */}
-        <Calendar/>
+         {/* <Navbar/> */}
+        {/* <Homepage/>
+        <Footer/>  */}
+        {/* <Calendar/> */}
+        <Student/>
       </>
       
     </div>
